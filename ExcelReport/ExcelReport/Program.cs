abad9d6f13800f06db39aa713556ac930d89bfc2
ADenisov <adenisov@moscow-cargo.com>
2018-01-25 11:14:14 +0300

    Изменения

--- ExcelReport/ExcelReport/Program.cs
+++ ExcelReport/ExcelReport/Program.cs
@@ -6,8 +6,10 @@ using System.Windows.Forms;
 
 namespace ExcelReport
 {
+    int a = 0;
     static class Program
     {
+        
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
